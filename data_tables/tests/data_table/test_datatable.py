43e0e4d63ce3b1c8b129971aa0c0cd2e92cc72c5
Nate Parsons <4307001+thehomebrewnerd@users.noreply.github.com>
2020-09-02 12:29:14 -0500

    Update and refactor initialization checks (#40)
    * init refactor
    
    * add check for duplicate col names
    
    * add check for unique index column
    
    * update unique column name check
    
    * update docstring

--- data_tables/tests/data_table/test_datatable.py
+++ data_tables/tests/data_table/test_datatable.py
@@ -2,6 +2,12 @@ import pandas as pd
 import pytest
 
 from data_tables import DataTable
+from data_tables.data_table import (
+    _check_index,
+    _check_time_index,
+    _check_unique_column_names,
+    _validate_params
+)
 
 
 def test_datatable_init(sample_df):
@@ -13,8 +19,16 @@ def test_datatable_init(sample_df):
     assert dt.time_index is None
     assert isinstance(df, pd.DataFrame)
     assert set(dt.columns.keys()) == set(sample_df.columns)
-    assert df is not sample_df
+    assert df is sample_df
     pd.testing.assert_frame_equal(df, sample_df)
+
+
+def test_datatable_copy_param(sample_df):
+    dt_with_copy = DataTable(sample_df, copy_dataframe=True)
+    assert sample_df is not dt_with_copy.df
+
+    dt_no_copy = DataTable(sample_df)
+    assert sample_df is dt_no_copy.df
 
 
 def test_datatable_init_with_name_and_index_vals(sample_df):
@@ -28,13 +42,49 @@ def test_datatable_init_with_name_and_index_vals(sample_df):
     assert dt.time_index == 'signup_date'
 
 
-def test_raises_error_with_invalid_dataframe_type():
+def test_validate_params_errors(sample_df):
     error_message = 'Dataframe must be a pandas.DataFrame'
-    with pytest.raises(AssertionError, match=error_message):
-        DataTable(pd.Series())
+    with pytest.raises(TypeError, match=error_message):
+        _validate_params(dataframe=pd.Series(),
+                         name=None,
+                         index=None,
+                         time_index=None)
 
-
-def test_raises_error_with_invalid_name_type(sample_df):
     error_message = 'DataTable name must be a string'
-    with pytest.raises(AssertionError, match=error_message):
-        DataTable(sample_df, name=1)
+    with pytest.raises(TypeError, match=error_message):
+        _validate_params(dataframe=sample_df,
+                         name=1,
+                         index=None,
+                         time_index=None)
+
+
+def test_check_index_errors(sample_df):
+    error_message = 'Index column name must be a string'
+    with pytest.raises(TypeError, match=error_message):
+        _check_index(dataframe=sample_df, index=1)
+
+    error_message = 'Specified index column `foo` not found in dataframe'
+    with pytest.raises(LookupError, match=error_message):
+        _check_index(dataframe=sample_df, index='foo')
+
+    error_message = 'Index column must be unique'
+    with pytest.raises(LookupError, match=error_message):
+        _check_index(sample_df, index='age')
+
+
+def test_check_time_index_errors(sample_df):
+    error_message = 'Time index column name must be a string'
+    with pytest.raises(TypeError, match=error_message):
+        _check_time_index(dataframe=sample_df,
+                          time_index=1)
+
+    error_message = 'Specified time index column `foo` not found in dataframe'
+    with pytest.raises(LookupError, match=error_message):
+        _check_time_index(dataframe=sample_df, time_index='foo')
+
+
+def test_check_unique_column_names(sample_df):
+    duplicate_cols_df = sample_df.copy()
+    duplicate_cols_df.insert(0, 'age', [18, 21, 65], allow_duplicates=True)
+    with pytest.raises(IndexError, match='Dataframe cannot contain duplicate columns names'):
+        _check_unique_column_names(duplicate_cols_df)
