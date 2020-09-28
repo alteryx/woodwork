09f74261ecf96e751fb68eb29d36c3ec92c6f7e6
Gaurav Sheni <gvsheni@gmail.com>
2020-09-28 16:11:40 -0400

    increment version, fix changelog

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='Alteryx, Inc.',
     author_email='support@featurelabs.com',
     license='BSD 3-clause',
-    version='0.0.1',
+    version='0.0.2',
     description='a two-dimensional data object with labeled axes and typing information',
     url='https://github.com/FeatureLabs/woodwork/',
     classifiers=[
