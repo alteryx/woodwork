5b1390e127f8080f014d5aaf0b3245c9abbbf0dc
Nate Parsons <nate.parsons@alteryx.com>
2020-09-28 12:25:36 -0500

    update conf.py to use woodwork

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,8 +27,8 @@ sys.path.insert(0, os.path.abspath(path))
 
 # -- Project information -----------------------------------------------------
 
-project = 'DataTables'
-copyright = '2019, Alteryx, Inc.'
+project = 'Woodwork'
+copyright = '2020, Alteryx, Inc.'
 author = 'Alteryx Innovation Labs'
 
 # The short X.Y version
@@ -128,7 +128,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ---------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'DataTablesdoc'
+htmlhelp_basename = 'Woodworkdoc'
 
 
 # -- Options for LaTeX output ------------------------------------------------
@@ -155,7 +155,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'DataTables.tex', 'DataTables Documentation',
+    (master_doc, 'Woodwork.tex', 'Woodwork Documentation',
      author, 'manual'),
 ]
 
@@ -165,7 +165,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'datatables', 'DataTables Documentation',
+    (master_doc, 'woodwork', 'Woodwork Documentation',
      [author], 1)
 ]
 
@@ -176,8 +176,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'DataTables', 'DataTables Documentation',
-     author, 'DataTables', 'One line description of project.',
+    (master_doc, 'Woodwork', 'Woodwork Documentation',
+     author, 'Woodwork', 'One line description of project.',
      'Miscellaneous'),
 ]
 
@@ -202,10 +202,10 @@ epub_exclude_files = ['search.html']
 
 # -- Extension configuration -------------------------------------------------
 
-# If datatables is open-sourced: replace github specific style.css
+# If woodwork is open-sourced: replace github specific style.css
 extlinks = {
-    'issue': ('https://github.com/Featurelabs/datatables/issues/%s', '#'),
-    'pr': ('https://github.com/Featurelabs/datatables/pull/%s', '#'),
+    'issue': ('https://github.com/Featurelabs/woodwork/issues/%s', '#'),
+    'pr': ('https://github.com/Featurelabs/woodwork/pull/%s', '#'),
     'user': ('https://github.com/%s', '@')
 }
 
