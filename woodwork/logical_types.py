cbb309f3c9dd200e0554b8c704e9e5767058718d
Nate Parsons <nate.parsons@alteryx.com>
2020-09-29 10:20:13 -0500

    add LogicalType docstrings

--- woodwork/logical_types.py
+++ woodwork/logical_types.py
@@ -2,8 +2,7 @@ from woodwork.utils import camel_to_snake
 
 
 class ClassNameDescriptor(object):
-    """Descriptor to convert a class's name from camelcase to snakecase
-    """
+    """Descriptor to convert a class's name from camelcase to snakecase"""
 
     def __get__(self, instance, class_):
         return camel_to_snake(class_.__name__)
@@ -16,6 +15,7 @@ class LogicalTypeMetaClass(type):
 
 
 class LogicalType(object, metaclass=LogicalTypeMetaClass):
+    """Base class for all other Logical Types"""
     type_string = ClassNameDescriptor()
     dtype = 'string'
     standard_tags = {}
@@ -25,10 +25,13 @@ class LogicalType(object, metaclass=LogicalTypeMetaClass):
 
 
 class Boolean(LogicalType):
+    """Represents a Logical type that contain values indicating true/false values"""
     pandas_dtype = 'boolean'
 
 
 class Categorical(LogicalType):
+    """Represents Logical Types that contain unordered discrete values that fall
+    into one of a set of possible values"""
     pandas_dtype = 'category'
     standard_tags = {'category'}
 
@@ -39,49 +42,66 @@ class Categorical(LogicalType):
 
 
 class CountryCode(LogicalType):
+    """Represents Logical Types that contain categorical information specifically
+    used to represent countries"""
     pandas_dtype = 'category'
     standard_tags = {'category'}
 
 
 class Datetime(LogicalType):
+    """Represents Logical Types that contain date and time information"""
     pandas_dtype = 'datetime64[ns]'
 
 
 class Double(LogicalType):
+    """Represents Logical Types that contain positive and negative numbers that
+    may include a fractional component, including zero (0)"""
     pandas_dtype = 'float64'
     standard_tags = {'numeric'}
 
 
 class Integer(LogicalType):
+    """Represents Logical Types that contain positive and negative numbers
+    without a fractional component, including zero (0)"""
     pandas_dtype = 'Int64'
     standard_tags = {'numeric'}
 
 
 class EmailAddress(LogicalType):
+    """Represents Logical Types that contain email address values"""
     pandas_dtype = 'string'
 
 
 class Filepath(LogicalType):
+    """Represents Logical Types that specify locations of files in a file system"""
     pandas_dtype = 'string'
 
 
 class FullName(LogicalType):
+    """Represents Logical Types that may contain first, middle and last names,
+    including honorifics and suffixes"""
     pandas_dtype = 'string'
 
 
 class IPAddress(LogicalType):
+    """Represents Logical Types that contain IP addresses, including both
+    IPv4 and IPv6 addresses"""
     pandas_dtype = 'string'
 
 
 class LatLong(LogicalType):
+    """Represents Logical Types that contain lattitude and longitude values"""
     pandas_dtype = 'string'
 
 
 class NaturalLanguage(LogicalType):
+    """Represents Logical Types that contain text or characters representing
+    natural human language"""
     pandas_dtype = 'string'
 
 
 class Ordinal(LogicalType):
+    """Represents Logical Types that contain ordered discrete values"""
     pandas_dtype = 'category'
     standard_tags = {'category'}
 
@@ -91,19 +111,26 @@ class Ordinal(LogicalType):
 
 
 class PhoneNumber(LogicalType):
+    """Represents Logical Types that contain numeric digits and characters
+    representing a phone number"""
     pandas_dtype = 'string'
 
 
 class SubRegionCode(LogicalType):
+    """Represents Logical Types that contain codes representing a portion of
+    a larger geographic region"""
     pandas_dtype = 'category'
     standard_tags = {'category'}
 
 
 class Timedelta(LogicalType):
+    """Represents Logical Types that contain values specifying a duration of time"""
     pandas_dtype = 'timedelta64[ns]'
 
 
 class URL(LogicalType):
+    """Represents Logical Types that contain URLs, which may include protocal, hostname
+    and file name"""
     pandas_dtype = 'string'
 
 
@@ -114,12 +141,13 @@ class WholeNumber(LogicalType):
 
 
 class ZIPCode(LogicalType):
+    """Represents Logical Types that contain a series of digits used for sorting mail"""
     pandas_dtype = 'category'
     standard_tags = {'category'}
 
 
 def get_logical_types():
-    '''Returns a dictionary of logical type name strings and logical type classes'''
+    """Returns a dictionary of logical type name strings and logical type classes"""
     # Get snake case strings
     logical_types = {logical_type.type_string: logical_type for logical_type in LogicalType.__subclasses__()}
     # Add class name strings
@@ -130,6 +158,7 @@ def get_logical_types():
 
 
 def str_to_logical_type(logical_str, raise_error=True):
+    """Helper function for converting a string value to the corresponding logical type object"""
     logical_str = logical_str.lower()
     logical_types_dict = {ltype_name.lower(): ltype for ltype_name, ltype in get_logical_types().items()}
 
