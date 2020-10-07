2a247e4335a8872ba6536ceff50233df82e4b526
Tamar Grey <64278226+tamargrey@users.noreply.github.com>
2020-10-07 11:51:03 -0400

    Add mutual info method on DataTable (#203)
    * Implement mutual info
    
    * Add docstrings
    
    * add data column checks for category and numeric
    
    * Clean up
    
    * Add handle nans test
    
    * Change purpose of helper
    
    * Add mutual information test
    
    * PR comments
    
    * Reorganize
    
    * Round mi in tests
    
    * More PR comments
    
    * Change nrows behavior to default to all rows

--- woodwork/tests/testing_utils/data_table_utils.py
+++ woodwork/tests/testing_utils/data_table_utils.py
@@ -8,3 +8,12 @@ def validate_subset_dt(subset_dt, dt):
         assert subset_col.semantic_tags == col.semantic_tags
         assert subset_col.dtype == col.dtype
         assert subset_col.to_pandas().equals(col.to_pandas())
+
+
+def mi_between_cols(col1, col2, df):
+    mi_series = df.loc[df['column_1'] == col1].loc[df['column_2'] == col2]['mutual_info']
+
+    if len(mi_series) == 0:
+        mi_series = df.loc[df['column_1'] == col2].loc[df['column_2'] == col1]['mutual_info']
+
+    return mi_series.iloc[0]
