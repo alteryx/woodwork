d6f8647d149abc912c3a3b2e7f606650bb192cb6
Tamar Grey <tamar.grey@alteryx.com>
2020-10-20 12:41:07 -0400

    Add ability to save metadata on disk

--- woodwork/serialize.py
+++ woodwork/serialize.py
@@ -5,21 +5,21 @@ from woodwork.utils import _get_ltype_class, _get_ltype_params
 from .version import __version__
 
 FEATURETOOLS_VERSION = '0.20.0'  # shouldnt have to hardcode this
-SCHEMA_VERSION = '0.1.0'
+SCHEMA_VERSION = '1.0.0'
 
 
 def datatable_to_metadata(datatable):
     dt_metadata = [
         {'name': col_name,
-            'nullable': col.to_pandas().isnull().any(),  # --> not sure this is what we mean by nullable
+            'nullable': bool(col.to_pandas().isnull().any()),  # --> not sure this is what we mean by nullable
             'ordinal': datatable.to_pandas().columns.get_loc(col_name),
             'logical_type': {
                 'parameters': _get_ltype_params(col.logical_type),
-                'type': _get_ltype_class(col.logical_type)
+                'type': str(_get_ltype_class(col.logical_type))
             },
             'physical_type': {
                 'parameters': {},  # --> col.to_pandas().dtype.__dict__ works for some but not all
-                'type': col.dtype
+                'type': str(col.dtype)
             },
             'tags': sorted(list(col.semantic_tags))}
         for col_name, col in datatable.columns.items()
@@ -32,3 +32,17 @@ def datatable_to_metadata(datatable):
         'schema_version': SCHEMA_VERSION,
         'metadata': dt_metadata
     }
+
+
+def write_data(datatable, path):
+    # get working locally and then add s3
+    path = os.path.abspath(path)
+    dump_table_metadata(datatable, path)
+
+
+def dump_table_metadata(datatable, path):
+    metadata = datatable_to_metadata(datatable)
+    file = os.path.join(path, 'table_metadata.json')
+
+    with open(file, 'w') as file:
+        json.dump(metadata, file)
