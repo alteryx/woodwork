2267ed30c7cbbf7b959722c6a5ba61e521fc2d2e
Tamar Grey <tamar.grey@alteryx.com>
2020-11-18 13:53:00 -0500

    fix lint

--- woodwork/tests/testing_utils/__init__.py
+++ woodwork/tests/testing_utils/__init__.py
@@ -1,2 +1,7 @@
 # flake8: noqa
-from .datatable_utils import check_column_order, mi_between_cols, to_pandas, validate_subset_dt
+from .datatable_utils import (
+    check_column_order,
+    mi_between_cols,
+    to_pandas,
+    validate_subset_dt
+)
