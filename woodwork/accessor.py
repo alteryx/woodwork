a3fe326381a4c15e894ddc82aa66ca99154262d4
Tamar Grey <tamar.grey@alteryx.com>
2020-12-18 13:58:58 -0500

    POC that automatically grabs all attrs old and new

---
+++ woodwork/accessor.py
@@ -0,0 +1,36 @@
+import woodwork as ww
+import pandas as pd
+
+
+@pd.api.extensions.register_dataframe_accessor("ww1")
+class DataTableAccessor:
+    def __init__(self, pandas_obj):
+        self._datatable = ww.DataTable(pandas_obj)
+
+    def __getattribute__(self, attr):
+        dt = object.__getattribute__(self, '_datatable')
+
+        if hasattr(dt, attr):
+            dt_attr = getattr(dt, attr)
+            if callable(dt_attr):
+                def wrapper(*args, **kwargs):
+                    return dt_attr(*args, **kwargs)
+                return wrapper
+            else:
+                return dt_attr
+        return object.__getattribute__(self, attr)
+
+    def init(self, **kwargs):
+        self._datatable = ww.DataTable(self._datatable.df, **kwargs)
+
+    @property
+    def dt(self):
+        return self._datatable
+
+    @dt.setter
+    def dt(self, datatable):
+        '''To be used any time a DataTable method returns a new DataTable object
+        '''
+        self._datatable = datatable
+
+# somehow setting an item doesnt call getattr so i need another way of intercepting it :/
