f1486e9465fe2f1552140cb5fce3f48874b5d67d
Tamar Grey <tamar.grey@alteryx.com>
2020-12-21 17:45:20 -0500

    Add DataColumn accessor

--- woodwork/accessor.py
+++ woodwork/accessor.py
@@ -1,26 +1,17 @@
 import woodwork as ww
 import dask.dataframe as dd
-from databricks.koalas.extensions import register_dataframe_accessor
+from databricks.koalas.extensions import register_dataframe_accessor, register_series_accessor
 import pandas as pd
 
 
-@pd.api.extensions.register_dataframe_accessor("ww")
+# DataFrame Accessors
+@pd.api.extensions.register_dataframe_accessor('ww')
 class DataTableAccessor:
     def __init__(self, pandas_obj):
         self._datatable = ww.DataTable(pandas_obj)
 
     def __getattribute__(self, attr):
-        dt = object.__getattribute__(self, '_datatable')
-
-        if hasattr(dt, attr):
-            dt_attr = getattr(dt, attr)
-            if callable(dt_attr):
-                def wrapper(*args, **kwargs):
-                    return dt_attr(*args, **kwargs)
-                return wrapper
-            else:
-                return dt_attr
-        return object.__getattribute__(self, attr)
+        return _get_attr_from_child(self, '_datatable', attr)
 
     def init(self, **kwargs):
         '''Initialize a DataTable the pandas accessor with any relevant DataTable arguments.
@@ -38,11 +29,61 @@ class DataTableAccessor:
         self._datatable = datatable
 
 
-@dd.extensions.register_dataframe_accessor("ww")
+@dd.extensions.register_dataframe_accessor('ww')
 class DaskDataTableAccessor(DataTableAccessor):
     pass
 
 
-@register_dataframe_accessor("ww")
+@register_dataframe_accessor('ww')
 class KoalasDataTableAccessor(DataTableAccessor):
     pass
+
+# DataColumn Accessors
+@pd.api.extensions.register_series_accessor('ww')
+class DataColumnAccessor:
+    def __init__(self, pandas_obj):
+        self._datacolumn = ww.DataColumn(pandas_obj)
+
+    def __getattribute__(self, attr):
+        return _get_attr_from_child(self, '_datacolumn', attr)
+
+    def init(self, **kwargs):
+        '''Initialize a DataColumn the pandas accessor with any relevant DataColumn arguments.
+        '''
+        self._datacolumn = ww.DataColumn(self._datacolumn.to_series(), **kwargs)
+
+    @property
+    def dc(self):
+        return self._datacolumn
+
+    @dc.setter
+    def dc(self, datacolumn):
+        '''To be used any time a DataColumn method returns a new DataColumn object
+        '''
+        self._datacolumn = datacolumn
+
+
+@dd.extensions.register_series_accessor('ww')
+class DaskDataColumnAccessor(DataColumnAccessor):
+    pass
+
+
+@register_series_accessor('ww')
+class KoalasDataColumnAccessor(DataColumnAccessor):
+    pass
+
+
+def _get_attr_from_child(obj, child, attr):
+    child = object.__getattribute__(obj, child)
+
+    if hasattr(child, attr):
+        child_attr = getattr(child, attr)
+        is_logical_type = ww.type_sys.utils._get_ltype_class(child_attr) in ww.logical_types.LogicalType.__subclasses__()
+
+        if callable(child_attr) and not is_logical_type:
+            def wrapper(*args, **kwargs):
+                return child_attr(*args, **kwargs)
+            return wrapper
+        else:
+            return child_attr
+    return object.__getattribute__(obj, attr)
