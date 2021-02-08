d50942f6e5589097b46ca51c0be680e54e371cb2
Tamar Grey <64278226+tamargrey@users.noreply.github.com>
2021-02-08 16:59:19 -0600

    Load demo data with accessor (#556)
    * load retail data and return dataframe with accessor
    
    * Docstring updates
    
    * Add release note
    
    * lint fix

--- woodwork/tests/demo_tests/test_retail.py
+++ woodwork/tests/demo_tests/test_retail.py
@@ -3,7 +3,7 @@ import urllib.request
 import pytest
 
 from woodwork import DataTable
-from woodwork.demo import load_retail
+from woodwork.demo import load_retail, load_retail_to_accessor
 from woodwork.logical_types import (
     Boolean,
     Categorical,
@@ -12,6 +12,7 @@ from woodwork.logical_types import (
     Integer,
     NaturalLanguage
 )
+from woodwork.schema import Schema
 
 
 @pytest.fixture(autouse=True)
@@ -58,3 +59,44 @@ def test_load_retail_datatable():
     assert dt.time_index == 'order_date'
     assert dt.columns['order_product_id'].semantic_tags == {'index'}
     assert dt.columns['order_date'].semantic_tags == {'time_index'}
+
+
+def test_load_retail_to_accessor_diff():
+    nrows = 10
+    df = load_retail_to_accessor(nrows=nrows, init_woodwork=False)
+    assert df.ww.schema is None
+    assert df.shape[0] == nrows
+    nrows_second = 11
+    df = load_retail_to_accessor(nrows=nrows_second, init_woodwork=False)
+    assert df.ww.schema is None
+    assert df.shape[0] == nrows_second
+
+    assert 'order_product_id' in df.columns
+    assert df['order_product_id'].is_unique
+
+
+def test_load_retail_to_accessor():
+    df = load_retail_to_accessor(nrows=10, init_woodwork=True)
+    assert isinstance(df.ww.schema, Schema)
+
+    expected_logical_types = {
+        'order_product_id': Categorical,
+        'order_id': Categorical,
+        'product_id': Categorical,
+        'description': NaturalLanguage,
+        'quantity': Integer,
+        'order_date': Datetime,
+        'unit_price': Double,
+        'customer_name': Categorical,
+        'country': Categorical,
+        'total': Double,
+        'cancelled': Boolean,
+    }
+
+    for column in df.ww.columns.values():
+        assert column['logical_type'] == expected_logical_types[column['name']]
+
+    assert df.ww.index == 'order_product_id'
+    assert df.ww.time_index == 'order_date'
+    assert df.ww.semantic_tags['order_product_id'] == {'index'}
+    assert df.ww.semantic_tags['order_date'] == {'time_index'}
