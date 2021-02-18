244020341082e1d931cd6bf4ac597f973c49c4ae
Tamar Grey <tamar.grey@alteryx.com>
2021-02-18 15:07:55 -0500

    pr comments

--- woodwork/table_accessor.py
+++ woodwork/table_accessor.py
@@ -67,9 +67,9 @@ class WoodworkTableAccessor:
                 specified logical type for the column. Defaults to True.
             column_descriptions (dict[str -> str], optional): Dictionary mapping column names to column descriptions.
             schema (Woodwork.Schema, optional): Typing information to use for the DataFrame instead of performing inference.
-                Any other arguments provided will be ignored. Note that any changes made to the typing information
-                after passing in the typing information will propogate to the original object. It is not advised to
-                share typing information between DataFrames.
+                Any other arguments provided will be ignored. Note that any changes made to the schema object after
+                initialization will propagate to the DataFrame. Similarly, to avoid unintended typing information changes,
+                the same schema object should not be shared between DataFrames.
         '''
         _validate_accessor_params(self._dataframe, index, make_index, time_index, logical_types, schema)
         if schema is not None:
