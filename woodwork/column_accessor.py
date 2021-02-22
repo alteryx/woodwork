b0e743f0c26fad436dfd9603798a2069c41698c9
Nate Parsons <nate.parsons@alteryx.com>
2021-02-22 13:43:32 -0600

    fix docs build

--- woodwork/column_accessor.py
+++ woodwork/column_accessor.py
@@ -91,23 +91,23 @@ class WoodworkColumnAccessor:
     def loc(self):
         """
         Access a group of rows and columns by label(s) or a boolean array.
+
         ``.loc[]`` is primarily label based, but may also be used with a
         boolean array.
+
         Allowed inputs are:
-        - A single label, e.g. ``5`` or ``'a'``, (note that ``5`` is
-          interpreted as a *label* of the index, and **never** as an
-          integer position along the index).
-        - A list or array of labels, e.g. ``['a', 'b', 'c']``.
-        - A slice object with labels, e.g. ``'a':'f'``.
-          .. warning:: Note that contrary to usual python slices, **both** the
-              start and the stop are included
-        - A boolean array of the same length as the axis being sliced,
-          e.g. ``[True, False, True]``.
-        - An alignable boolean Series. The index of the key will be aligned before
-          masking.
-        - An alignable Index. The Index of the returned selection will be the input.
-        - A ``callable`` function with one argument (the calling Series or
-          DataFrame) and that returns valid output for indexing (one of the above)
+            A single label, e.g. ``5`` or ``'a'``, (note that ``5`` is
+            interpreted as a *label* of the index, and **never** as an
+            integer position along the index).
+            A list or array of labels, e.g. ``['a', 'b', 'c']``.
+            A slice object with labels, e.g. ``'a':'f'``.
+            A boolean array of the same length as the axis being sliced,
+            e.g. ``[True, False, True]``.
+            An alignable boolean Series. The index of the key will be aligned before
+            masking.
+            An alignable Index. The Index of the returned selection will be the input.
+            A ``callable`` function with one argument (the calling Series or
+            DataFrame) and that returns valid output for indexing (one of the above)
         """
         return _locIndexerAccessor(self._series)
 
