0a2cead3dc901aab17c7e162a200f6c0656866e8
Tamar Grey <64278226+tamargrey@users.noreply.github.com>
2021-03-12 17:39:18 -0500

    Include index and allow unique columns in Accessor mutual info (#699)
    * Include unique columns in mutual info calculationms
    
    * Add include_index param to mi
    
    * add release note

--- woodwork/tests/accessor/test_statistics.py
+++ woodwork/tests/accessor/test_statistics.py
@@ -127,7 +127,8 @@ def test_mutual_info(df_mi):
     pd.testing.assert_frame_equal(mi, mi_many_rows)
 
     mi = df_mi.ww.mutual_information(nrows=1)
-    assert mi.shape[0] == 0
+    assert mi.shape[0] == 10
+    assert (mi['mutual_info'] == 1.0).all()
 
     mi = df_mi.ww.mutual_information(num_bins=2)
     assert mi.shape[0] == 10
@@ -141,11 +142,14 @@ def test_mutual_info(df_mi):
     pd.testing.assert_frame_equal(to_pandas(df_mi), to_pandas(original_df))
 
 
-def test_mutual_info_does_not_include_index(sample_df):
+def test_mutual_info_on_index(sample_df):
     sample_df.ww.init(index='id')
     mi = sample_df.ww.mutual_information()
 
-    assert 'id' not in mi['column_1'].values
+    assert not ('id' in mi['column_1'].values or 'id' in mi['column_2'].values)
+
+    mi = sample_df.ww.mutual_information(include_index=True)
+    assert 'id' in mi['column_1'].values or 'id' in mi['column_2'].values
 
 
 def test_mutual_info_returns_empty_df_properly(sample_df):
@@ -177,8 +181,8 @@ def test_mutual_info_unique_cols(df_mi_unique):
     mi = df_mi_unique.ww.mutual_information()
 
     cols_used = set(np.unique(mi[['column_1', 'column_2']].values))
-    assert 'unique' not in cols_used
-    assert 'unique_with_one_nan' not in cols_used
+    assert 'unique' in cols_used
+    assert 'unique_with_one_nan' in cols_used
     assert 'unique_with_nans' in cols_used
     assert 'ints' in cols_used
 
