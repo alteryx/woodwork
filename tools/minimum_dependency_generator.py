246a63bd0793d6c70e55fc94a6236d68c97cb55d
Gaurav Sheni <gvsheni@gmail.com>
2021-03-28 21:41:43 -0400

    add koalas test

--- tools/minimum_dependency_generator.py
+++ tools/minimum_dependency_generator.py
@@ -1,4 +1,5 @@
 import argparse
+from collections import defaultdict
 import re
 
 import requests
@@ -11,10 +12,12 @@ def versions(package_name):
     url = "https://pypi.org/pypi/%s/json" % (package_name,)
     r = requests.get(url)
     data = r.json()
-    releases = data["releases"].keys()
-    releases = [x for x in list(releases) if 'rc' not in x and 'b' not in x and 'post' not in x]
-    versions = list(releases)
-    versions = [Version(x) for x in versions]
+    releases = []
+    for rel in list(data["releases"].keys()):
+        for avoid_tag  in ['rc', 'b', 'post']:
+            if rel not in avoid_tag:
+                releases.append(rel)
+    versions = [Version(x) for x in releases]
     versions.sort(reverse=False)
     return versions
 
@@ -34,15 +37,28 @@ def verify_python_environment(requirement):
         # no python version specified in requirement
         return True
     elif package.marker and package.marker.evaluate():
-        # evaluate --> Return the boolean from evaluating the given marker against the current Python process environment
+        # evaluate --> evaluating the given marker against the current Python process environment
         return True
     return False
 
 
-def find_min_requirement(requirement):
+def remove_comment(requirement):
     if '#' in requirement:
         # remove everything after comment character
         requirement = requirement.split('#')[0]
+    return requirement
+
+
+def generate_package_name(requirement):
+    package = requirement
+    if not isinstance(package, Requirement):
+        package = Requirement(requirement)
+    if len(package.extras):
+        return package.name + "[" + package.extras.pop() + "]"
+    return package.name
+
+def find_min_requirement(requirement):
+    requirement = remove_comment(requirement)
     if not verify_python_environment(requirement):
         return None
     if '>=' in requirement:
@@ -79,25 +95,35 @@ def find_min_requirement(requirement):
     return Requirement(package.name + str(mininum))
 
 
-def write_min_requirements(requirements_path, output_path):
-    all_requirements = []
+def write_min_requirements(output_path, requirements_paths):
+    requirements_to_specifier = defaultdict(list)
     min_requirements = []
 
-    with open(requirements_path) as f:
-        all_requirements = f.readlines()
+    for path in requirements_paths:
+        requirements = []
+        with open(path) as f:
+            requirements.extend(f.readlines())
+        for req in requirements:
+            package = Requirement(remove_comment(req))
+            name = generate_package_name(package)
+            if name in requirements_to_specifier:
+                prev_req = Requirement(requirements_to_specifier[name])
+                new_req = prev_req.specifier & package.specifier
+                requirements_to_specifier[name] = package.name + str(new_req)
+            else:
+                requirements_to_specifier[name] = req
 
-    for req in all_requirements:
+    for req in list(requirements_to_specifier.values()):
         min_version = find_min_requirement(req)
         min_requirements.append(str(min_version) + "\n")
 
-    print(min_requirements)
     with open(output_path, "w") as f:
         f.writelines(min_requirements)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="reads a requirements file and outputs the minimized requirements")
-    parser.add_argument('requirements_path', help='path for requirements to minimize')
     parser.add_argument('output_path', help='path to output minimized requirements')
+    parser.add_argument('--requirements_paths', nargs='+', help='path for requirements to minimize', required=True)
     args = parser.parse_args()
-    write_min_requirements(args.requirements_path, args.output_path)
+    write_min_requirements(args.output_path, args.requirements_paths)
