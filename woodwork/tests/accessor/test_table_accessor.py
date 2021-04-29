60567a3d8525217c7c650e385d83187d79e21d27
Frances Hartwell <frances.hartwell@alteryx.com>
2021-04-29 16:05:39 -0400

    remove reference to to_csv, to_parquet, to_pickle from tests

--- woodwork/tests/accessor/test_table_accessor.py
+++ woodwork/tests/accessor/test_table_accessor.py
@@ -201,10 +201,7 @@ def test_accessor_init_errors_methods(sample_df):
         'set_time_index': ['signup_date'],
         'set_types': [{'id': 'Integer'}],
         'to_disk': ['dir'],
-        'to_csv': ['dir'],
         'to_dictionary': None,
-        'to_parquet': ['dir'],
-        'to_pickle': ['dir'],
         'value_counts': None,
 
     }
