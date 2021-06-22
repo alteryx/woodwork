bacc9345b4a20ae6b40fdbbfef8eddd2805af63e
Frances Hartwell <frances.hartwell@alteryx.com>
2021-06-22 19:20:10 -0400

    Update collections import to avoid deprecation warning with python 3.8 (#1010)
    * update collections to avoid warning with python 3.8
    
    * release notes
    
    * lint
    
    * Update table_schema.py
    
    * better import
    
    Co-authored-by: Gaurav Sheni <gvsheni@gmail.com>

--- woodwork/table_schema.py
+++ woodwork/table_schema.py
@@ -1,5 +1,5 @@
-import collections
 import copy
+from collections.abc import Hashable
 
 import pandas as pd
 
@@ -459,7 +459,7 @@ class TableSchema(object):
                 raise TypeError(f"Specified LogicalType selector {maybe_ltype} is not registered in Woodwork's type system.")
 
             # Hashability as a proxy for whether a selector is possibly a semantic tag or column name
-            if not isinstance(selector, collections.Hashable):
+            if not isinstance(selector, Hashable):
                 raise TypeError(f"Invalid selector used in include: {selector} must be a "
                                 "string, uninstantiated and registered LogicalType, or valid column name")
             # Determine if the selector is a semantic tag
