7188dab7a8624e8fbf638dd5ecf41f13e98e27c5
Nate Parsons <nate.parsons@alteryx.com>
2021-11-29 13:43:40 -0600

    update comment

--- woodwork/statistics_utils.py
+++ woodwork/statistics_utils.py
@@ -150,7 +150,8 @@ def _get_describe_dict(
                 values["histogram"] = _get_histogram_values(series, bins=bins)
                 _range = range(int(values["min"]), int(values["max"]) + 1)
                 # Calculate top numeric values if range of values present
-                # is less than or equal number of histogram bins
+                # is less than or equal number of histogram bins and series
+                # contains only integer values
                 if len(_range) <= bins and (series % 1 == 0).all():
                     values["top_values"] = _get_numeric_value_counts_in_range(
                         series, _range
