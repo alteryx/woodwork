ee5f5c7b4f5fe88688564d390b9d66e43e34ca1e
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2022-01-05 18:01:57 -0600

    add test case

--- woodwork/tests/accessor/test_table_accessor.py
+++ woodwork/tests/accessor/test_table_accessor.py
@@ -2964,3 +2964,8 @@ def test_falsy_columns_in_partial_schema(falsy_names_df):
     assert new_df.ww.index == 0
     assert new_df.ww.time_index == ""
     assert new_df.ww.name == 0
+
+
+def test_nan_index_error(sample_df_pandas):
+    with pytest.raises(IndexError, match="Index contains null values"):
+        sample_df_pandas.ww.init(index="email")
