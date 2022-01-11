1f71e51d3913ad1d3c95258cd686705e31d11afc
Nate Parsons <nate.parsons@alteryx.com>
2022-01-11 09:58:51 -0600

    fix for optional dask

--- woodwork/deserialize.py
+++ woodwork/deserialize.py
@@ -7,14 +7,15 @@ import warnings
 from itertools import zip_longest
 from pathlib import Path
 
-import dask.dataframe as dd
 import pandas as pd
 
 import woodwork as ww
 from woodwork.exceptions import OutdatedSchemaWarning, UpgradeSchemaWarning
 from woodwork.s3_utils import get_transport_params, use_smartopen
 from woodwork.serialize import FORMATS, SCHEMA_VERSION
-from woodwork.utils import _is_s3, _is_url, import_or_raise
+from woodwork.utils import _is_s3, _is_url, import_or_none, import_or_raise
+
+dd = import_or_none("dask.dataframe")
 
 
 def read_table_typing_information(path):
