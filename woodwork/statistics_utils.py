54d5ef6828fd32d1cb789c4d4c4e11a95628b8fe
Tamar Grey <tamar.grey@alteryx.com>
2022-01-27 11:52:55 -0500

    Update docstring

--- woodwork/statistics_utils.py
+++ woodwork/statistics_utils.py
@@ -463,10 +463,10 @@ def _get_box_plot_info_for_column(
             Defaults to True.
 
     Note:
-        The minimum quantiles necessary for outlier detection using the IQR method are the
-        first quartile (0.25) and third quartile (0.75). If these keys are missing from the quantiles dictionary,
-        the following quantiles will be calculated: {0.0, 0.25, 0.5, 0.75, 1.0}, which correspond to
-        {min, first quantile, median, third quantile, max}.
+        The minimum quantiles necessary for building a box ploy using the IQR method are the
+        minimum value (0.0 in the quantiles dict), first quartile (0.25), third quartile (0.75), and maximum value (1.0).
+        If no quantiles are providex, the following quantiles will be calculated:
+        {0.0, 0.25, 0.5, 0.75, 1.0}, which correspond to {min, first quantile, median, third quantile, max}.
 
     Returns:
         (dict[str -> float,list[number]]): Returns a dictionary containing box plot information for the Series.
