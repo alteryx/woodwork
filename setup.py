84b49280cc9d856144b7e889abf21bbb7908ed7e
Gaurav Sheni <gvsheni@gmail.com>
2022-02-07 10:37:05 -0500

    initial setup

--- setup.py
+++ setup.py
@@ -1,42 +1,5 @@
-from os import path
+#!/usr/bin/env python
+import setuptools
 
-from setuptools import find_packages, setup
-
-dirname = path.abspath(path.dirname(__file__))
-with open(path.join(dirname, "README.md")) as f:
-    long_description = f.read()
-
-extras_require = {
-    "dask": open("dask-requirements.txt").readlines(),
-    "koalas": open("koalas-requirements.txt").readlines(),
-    "update_checker": ["alteryx-open-src-update-checker >= 2.0.0"],
-}
-extras_require["complete"] = sorted(set(sum(extras_require.values(), [])))
-
-setup(
-    name="woodwork",
-    author="Alteryx, Inc.",
-    author_email="open_source_support@alteryx.com",
-    license="BSD 3-clause",
-    version="0.12.0",
-    description="a two-dimensional data object with labeled axes and typing information",
-    url="https://github.com/alteryx/woodwork/",
-    classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
-    packages=find_packages(),
-    install_requires=open("requirements.txt").readlines(),
-    tests_require=open("test-requirements.txt").readlines(),
-    python_requires=">=3.8, <4",
-    extras_require=extras_require,
-    keywords="data science machine learning typing",
-    include_package_data=True,
-    entry_points={"console_scripts": ["woodwork = woodwork.__main__:cli"]},
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    data_files=[("woodwork/data", ["woodwork/data/1-1000.txt"])],
-)
+if __name__ == "__main__":
+    setuptools.setup()
