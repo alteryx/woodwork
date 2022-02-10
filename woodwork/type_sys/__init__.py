3116b9ed582fe937a61f6f67ab6e2da93d3ad7ad
Nate Parsons <nate.parsons@alteryx.com>
2022-02-10 11:36:40 -0600

    put warning in correct location

--- woodwork/type_sys/__init__.py
+++ woodwork/type_sys/__init__.py
@@ -1,9 +1,1 @@
-import sys
-import warnings
-
 from .type_system import type_system
-
-if sys.version_info.major == 3 and sys.version_info.minor == 7:  # pragma: no cover
-    warnings.warn(
-        "Woodwork may not support Python 3.7 in next non-bugfix release.", FutureWarning
-    )
