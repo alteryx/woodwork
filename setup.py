1f1b13a5802b536397cd36cc1d0eb162cc9849d3
ming.qi <ming.qi@alteryx.com>
2022-02-23 18:37:51 -0500

    Add Python 3.10 support. Issue #1212

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     packages=find_packages(),
     install_requires=open("requirements.txt").readlines(),
