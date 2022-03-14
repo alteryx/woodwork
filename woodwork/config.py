3f211f65e44afffad53765213d768f4594273f8d
Dave Reed <dave.reed@alteryx.com>
2022-03-14 10:21:24 -0400

    added constants to config

--- woodwork/config.py
+++ woodwork/config.py
@@ -1,4 +1,5 @@
 import contextlib
+from woodwork.statistics_utils.frequency_inference import _constants as FREQUENCY_INFERENCE_CONSTANTS
 
 CONFIG_DEFAULTS = {
     "categorical_threshold": 0.2,
@@ -18,6 +19,8 @@ CONFIG_DEFAULTS = {
 """.splitlines()
     ),
     "nan_values": ["", "None", "nan", "NaN", "<NA>", "null"],
+    "frequence_inference_window_length": FREQUENCY_INFERENCE_CONSTANTS.WINDOW_LENGTH,
+    "frequence_inference_threshold": FREQUENCY_INFERENCE_CONSTANTS.FREQ_INFERENCE_THRESHOLD
 }
 
 
