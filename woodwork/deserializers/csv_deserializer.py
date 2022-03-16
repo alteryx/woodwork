a57c5a7d17c50f4b22559471d36e6226bc03d28e
Jeff Hernandez <12969559+jeff-hernandez@users.noreply.github.com>
2022-03-16 09:58:06 -0500

    use pyarrow engine in pandas

--- woodwork/deserializers/csv_deserializer.py
+++ woodwork/deserializers/csv_deserializer.py
@@ -1,4 +1,5 @@
 from woodwork.deserializers.deserializer_base import Deserializer
+from woodwork.utils import import_or_none
 
 
 class CSVDeserializer(Deserializer):
@@ -8,4 +9,14 @@ class CSVDeserializer(Deserializer):
 
     def read_from_local_path(self):
         lib = self._get_library()
+        self._use_pyarrow_engine()
         return lib.read_csv(self.read_path, dtype=self.column_dtypes, **self.kwargs)
+
+    def _use_pyarrow_engine(self):
+        """Adds pyarrow engine to kwargs to improve performance in pandas."""
+        if (
+            "pandas" == self.typing_info["loading_info"]["table_type"]
+            and bool(import_or_none("pyarrow.parquet"))
+            and "engine" not in self.kwargs
+        ):
+            self.kwargs.update(engine="pyarrow")
