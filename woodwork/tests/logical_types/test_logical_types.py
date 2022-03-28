a1c585bb16c7dd7981687c8e07d94c9b7d39a96a
Parthiv Naresh <parthiv.naresh@alteryx.com>
2022-03-28 16:48:16 -0400

    separate more complex test

--- woodwork/tests/logical_types/test_logical_types.py
+++ woodwork/tests/logical_types/test_logical_types.py
@@ -319,15 +319,11 @@ def test_age_validate(sample_df, logical_type):
     assert to_pandas(actual).equals(to_pandas(invalid_row))
 
 
-def test_phone_number_validate(sample_df_phone_numbers):
+def test_phone_number_validate(sample_df):
     phone_number = PhoneNumber()
     dtype = phone_number.primary_dtype
-    series = sample_df_phone_numbers["phone_number"].astype(dtype)
-    # Current inference function does not match lack of area code, invalid US phone number
-    invalid_row = pd.Series(
-        {17: "252 9384", 18: "+1 194 129 1991", 19: "+001 236 248 8482"},
-        name="phone_number",
-    ).astype(dtype)
+    series = sample_df["phone_number"].astype(dtype)
+    invalid_row = pd.Series({4: "bad_phone"}, name="phone_number").astype(dtype)
 
     if _is_spark_series(series):
         invalid_row = ps.from_pandas(invalid_row)
@@ -341,6 +337,22 @@ def test_phone_number_validate(sample_df_phone_numbers):
     with pytest.raises(TypeValidationError, match=match):
         phone_number.validate(series)
 
+    actual = phone_number.validate(series, return_invalid_values=True)
+    expected = pd.Series({4: "bad_phone"}, name="phone_number").astype(dtype)
+    assert to_pandas(actual).equals(expected)
+
+
+def test_phone_number_validate_complex(sample_df_phone_numbers):
+    phone_number = PhoneNumber()
+    dtype = phone_number.primary_dtype
+    series = sample_df_phone_numbers["phone_number"].astype(dtype)
+    # Current inference function does not match lack of area code
+    invalid_row = pd.Series(
+        {17: "252 9384", 18: "+1 194 129 1991", 19: "+001 236 248 8482"},
+        name="phone_number",
+    ).astype(dtype)
+
+    series = series.append(invalid_row).astype(dtype)
     actual = phone_number.validate(series, return_invalid_values=True)
     expected = pd.Series(
         {17: "252 9384", 18: "+1 194 129 1991", 19: "+001 236 248 8482"},
