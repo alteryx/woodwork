b32822297e4b09c13121c84526e93ee9f379e1ba
bchen1116 <bchen1116@gmail.com>
2022-08-31 16:53:27 -0400

    add part

--- woodwork/tests/logical_types/test_logical_types.py
+++ woodwork/tests/logical_types/test_logical_types.py
@@ -850,7 +850,7 @@ def test_pyspark_dask_series(type):
             "dask.dataframe",
             reason="Dask not installed, skipping",
         )
-        df = dd.from_pandas(df)
+        df = dd.from_pandas(df, npartitions=2)
     df.ww.init(logical_types={"dates": Datetime})
     df_expected = pd.DataFrame({"dates": expected_values}, dtype="datetime64[ns]")
     if type == "pyspark":
