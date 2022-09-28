0e74e64657386e47332cf11b7a0cee100352fad1
sbadithe <shripad.badithe@alteryx.com>
2022-09-28 13:42:20 -0700

    Change test_numeric_histogram to remove failing string test

--- woodwork/tests/accessor/test_statistics.py
+++ woodwork/tests/accessor/test_statistics.py
@@ -1523,6 +1523,7 @@ def test_datetime_get_recent_value_counts():
 
 def test_numeric_histogram():
     column = pd.Series(np.random.randn(1000))
+    column = pd.concat([column, pd.Series([np.nan])])
     bins = 7
     values = _get_histogram_values(column, bins=bins)
     assert len(values) == bins
