cd656d0fdbc611b29278a189aeb88f89c5459c24
Parthiv Naresh <parthiv.naresh@alteryx.com>
2022-11-02 13:54:16 +0530

    lint

--- woodwork/tests/speed_test.py
+++ woodwork/tests/speed_test.py
@@ -1,15 +1,17 @@
-import cProfile, pstats
-import numpy as np
+import cProfile
+import pstats
 import time
+
+import numpy as np
 import pandas as pd
 
 from woodwork import init_series
 
 
 def kdd():
-    #col = np.array([1] * 2 + [2] * 26 + [3] * 9 + [4] * 2 + [5] * 2 + [6] * 2 + [7] * 3 + [8, 8, 9, 9, 10, 11, 13, 16, 21, 28])
-    #col = np.repeat(col, 8000)
-    #col = pd.Series(col)
+    # col = np.array([1] * 2 + [2] * 26 + [3] * 9 + [4] * 2 + [5] * 2 + [6] * 2 + [7] * 3 + [8, 8, 9, 9, 10, 11, 13, 16, 21, 28])
+    # col = np.repeat(col, 8000)
+    # col = pd.Series(col)
     np.random.seed(42)
     df = pd.read_csv("/Users/parthiv.naresh/Documents/Datasets/stones_encoded.csv")
     col = df["meas_depth"]
@@ -19,12 +21,12 @@ def kdd():
     print(mc_results)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     profiler = cProfile.Profile()
     profiler.enable()
     kdd()
     profiler.disable()
     stats = pstats.Stats(profiler)
-    sorted_stats = stats.sort_stats('cumtime')
+    sorted_stats = stats.sort_stats("cumtime")
     sorted_stats.print_stats(50)
-    stats.dump_stats('/Users/parthiv.naresh/0_16_4_stat.txt')
+    stats.dump_stats("/Users/parthiv.naresh/0_16_4_stat.txt")
