0b76dbd39ce6479a2661d247bc5438be98532ad8
sbadithe <shripad.badithe@alteryx.com>
2022-11-09 10:50:14 -0800

    Use existing fixture and refactor test

--- woodwork/tests/type_system/conftest.py
+++ woodwork/tests/type_system/conftest.py
@@ -332,25 +332,7 @@ def pdnas(request):
     return request.getfixturevalue(request.param)
 
 
-# Empty Inference Fixtures
-@pytest.fixture
-def pandas_empty():
-    return pd.Series([], dtype="object")
 
-
-@pytest.fixture
-def dask_empty(pandas_empty):
-    return pd_to_dask(pandas_empty)
-
-
-@pytest.fixture
-def pyspark_empty(pandas_empty):
-    return pd_to_spark(pandas_empty)
-
-
-@pytest.fixture(params=["pandas_empty", "dask_empty", "pyspark_empty"])
-def empty_series(request):
-    return request.getfixturevalue(request.param)
 
 
 # Null Inference Fixtures
