bfaeab86c03e31bf5629eef21475acb1acf4c0f7
Gaurav Sheni <gaurav.sheni@alteryx.com>
2022-12-12 15:50:08 -0500

    change error

--- woodwork/utils.py
+++ woodwork/utils.py
@@ -669,5 +669,5 @@ def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
     for member in tar.getmembers():
         member_path = os.path.join(path, member.name)
         if not is_within_directory(path, member_path):
-            raise Exception("Attempted Path Traversal in Tar File")
+            raise ValueError("Attempted Path Traversal in Tar File")
     tar.extractall(path, members, numeric_owner=numeric_owner)
